1051ef2c885cc1a6f72aae3113af29194de45c64
Igor-AZ <igor.apfentaler@autozubak.com>
2019-11-25 13:28:27 +0100

    Content + observers

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,29 @@ for (let i = 0; i < svgText.lenght; i++) {
 // var colors = ["#74B087","#DE7300","#74B087"];
 
 // function animate() {};
+
+
+// Navigation observers
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
